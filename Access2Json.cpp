08cc30eba08f16fcd857db3228d00bd6874f6e5f
Daniel Anderson <wonixen@gmail.com>
2021-12-12 11:32:32 -0500

    handling of codepage and exception when bad conversion with row data

--- Access2Json.cpp
+++ Access2Json.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <format>
 #include <fstream>
 #include <iostream>
 #include <variant>
@@ -19,206 +20,269 @@
 
 namespace json = boost::json;
 
-struct TableExport {
-  std::string name;
-  std::string extractQry;
+struct TableExport
+{
+   std::string name;
+   std::string extractQry;
 };
 
-std::vector<TableExport> g_tablesToExport = {
-    {"Tags", "SELECT  * FROM Tags ORDER BY Tag_Code"},
-    {"Fields", "SELECT  * FROM Fields ORDER BY Msg_Code, Tag_Code"},
-    {"LogFiles", "SELECT  * FROM LogFiles ORDER BY Log_Code"},
-    {"Messages", "SELECT  * FROM Messages ORDER BY Msg_Code"},
-    {"LoggerMessages", "SELECT  Log_Code, Msg_Code, Schema FROM LoggerMessages "
-                       "WHERE  Header_Msg_Code IS NULL ORDER BY Schema, "
-                       "Log_Code, Msg_Code"},
-    {"HeaderMessages", "SELECT  Header_Msg_Code as Header_Code, Schema, "
-                       "Log_Code, Msg_Code FROM LoggerMessages WHERE  "
-                       "Header_Msg_Code IS NOT NULL ORDER BY Header_Msg_Code, "
-                       "Schema, Log_Code, Msg_Code"},
+std::vector<TableExport> g_tablesToExport =
+   {
+      {"Tags", "SELECT * FROM Tags ORDER BY Tag_Code"},
+      {"Fields", "SELECT * FROM Fields ORDER BY Msg_Code, Tag_Code"},
+      {"LogFiles", "SELECT * FROM LogFiles ORDER BY Log_Code"},
+      {"Messages", "SELECT * FROM Messages ORDER BY Msg_Code"},
+      {"LoggerMessages", "SELECT * FROM LoggerMessages ORDER BY Schema, Log_Code, Msg_Code"},
 };
 
 // dumping row to std::cerr for debugging purposes!
 // not use anymore
-void DumpResult(nanodbc::result &result, short BadCol) {
-  for (int i = 0; i < result.columns(); i++) {
-    std::cerr << result.column_name(i) << "[";
-    if (result.is_bound(i))
-      std::cerr << ((result.is_null(i))
-                        ? std::string{"NULL"}
-                        : ("'" + result.get<nanodbc::string>(i) + "'"));
-    std::cerr << "] ";
-  }
-  std::cerr << "\n error with: " << result.column_name(BadCol) << std::endl;
+void DumpResult(nanodbc::result& result, short BadCol)
+{
+   for (int i = 0; i < result.columns(); i++)
+   {
+      std::cerr << result.column_name(i) << "[";
+      if (result.is_bound(i))
+         std::cerr << ((result.is_null(i)) ? std::string {"NULL"} : ("'" + result.get<nanodbc::string>(i) + "'"));
+      std::cerr << "] ";
+   }
+   std::cerr << "\n error with: " << result.column_name(BadCol) << std::endl;
 }
 
-struct ToJsonValue {
-  json::value &_v;
+struct ToJsonValue
+{
+   json::value& _v;
 
-  ToJsonValue(json::value &v) : _v(v) {}
-  void operator()(void *) const { _v = nullptr; }
-  void operator()(int64_t i) const { _v = i; }
-  void operator()(double d) const { _v = d; }
-  void operator()(const std::string &s) const { _v = s; }
-  void operator()(const std::vector<uint8_t> &v) const {
-    _v = json::value_from(v);
-  }
+   ToJsonValue(json::value& v) :
+      _v(v) {}
+   void operator()(void*) const { _v = nullptr; }
+   void operator()(int64_t i) const { _v = i; }
+   void operator()(double d) const { _v = d; }
+   void operator()(const std::string& s) const { _v = s; }
+   void operator()(const std::vector<uint8_t>& v) const
+   {
+      _v = json::value_from(v);
+   }
 };
 
-struct ToJsonArray {
-  json::array &_a;
+struct ToJsonArray
+{
+   json::array& _a;
 
-  ToJsonArray(json::array &a) : _a(a) {}
-  void operator()(void *) const { _a.push_back(nullptr); }
-  void operator()(int64_t i) const { _a.push_back(i); }
-  void operator()(double d) const { _a.push_back(d); }
-  void operator()(const std::string &s) const {
-    json::value v;
+   ToJsonArray(json::array& a) :
+      _a(a) {}
+   void operator()(void*) const { _a.push_back(nullptr); }
+   void operator()(int64_t i) const { _a.push_back(i); }
+   void operator()(double d) const { _a.push_back(d); }
+   void operator()(const std::string& s) const
+   {
+      json::value v;
 
-    v = s;
-    _a.push_back(v);
-  }
-  void operator()(const std::vector<uint8_t> &v) const {
-    _a.push_back(json::value_from(v));
-  }
+      v = s;
+      _a.push_back(v);
+   }
+   void operator()(const std::vector<uint8_t>& v) const
+   {
+      _a.push_back(json::value_from(v));
+   }
 };
 
-using DbValue =
-    std::variant<void *, int64_t, std::string, double, std::vector<uint8_t>>;
+using DbValue = std::variant<void*, int64_t, std::string, double, std::vector<uint8_t>>;
 
-DbValue GetColumnValue(nanodbc::result &row, short col) {
-  DbValue dbData;
-  if (row.is_bound(col) && row.is_null(col))
-    return dbData;
+DbValue GetColumnValue(nanodbc::result& row, short col)
+{
+   // for string conversion, msaccess uses:
+   // HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Nls\CodePage
+   // which in tmx case is codepage 1252
 
-  // for long data type, must fetch then test for NULL
-  // for short data type, we must test for null before fetching!
-  switch (row.column_datatype(col)) {
-  case SQL_LONGVARBINARY: {
-    // Unbound data can only be tested for null once fetched
-    auto blob = row.get<std::vector<uint8_t>>(col); // try to fetch blob
-    if (row.is_null(col) == false)                  // did we get data ?
-      dbData = blob;
-    return dbData;
-  }
+   // only when the row is bound can we test for NULL!
+   if (row.is_bound(col) && row.is_null(col))
+      return DbValue {};
 
-  case SQL_LONGVARCHAR: {
-    // Unbound data can only be tested for null once fetched
-    auto longText = row.get<nanodbc::string>(col); // try to fetch long text
-    if (row.is_null(col) == false)                 // got something!!!
-      dbData = Cp1252ToUtf8(longText); // long text appear as codepage 1252,
-                                       // convert to utf8 which is what json
-                                       // understand
-    return dbData;
-  }
+   // for long data type, must fetch then test for NULL
+   // for short data type, we must test for null before fetching!
+   switch (row.column_datatype(col))
+   {
+      case SQL_LONGVARBINARY:
+      {
+         // Unbound data can only be tested for null once fetched
+         auto blob = row.get<std::vector<uint8_t>>(col);   // try to fetch blob
+         if (row.is_null(col) == false)                    // did we get data ?
+         {
+            return blob;
+         }
+         return DbValue {};
+      }
 
-  case SQL_BIGINT:
-  case SQL_TINYINT:
-  case SQL_INTEGER:
-  case SQL_SMALLINT:
-    dbData = row.get<std::int64_t>(col);
-    return dbData;
+      // for MsAccess, SQL_CHAR is CP1252
+      case SQL_VARCHAR:
+      case SQL_CHAR:
+         return Cp1252ToUtf8(row.get<nanodbc::string>(col));
 
-  case SQL_VARCHAR:
-  case SQL_CHAR:
-    dbData = WStringToUtf8(row.get<nanodbc::wide_string>(col));
-    return dbData;
+      case SQL_LONGVARCHAR:
+      {
+         auto varchar = row.get<nanodbc::string>(col);
 
-  case SQL_NUMERIC:
-  case SQL_DECIMAL:
-  case SQL_FLOAT:
-  case SQL_REAL:
-  case SQL_DOUBLE:
-    dbData = row.get<double>(col);
-    return dbData;
+         // only is it's not NULL can we use it, without the test the NULL column
+         // would be converted to empty string!!!
+         if (row.is_null(col) == false)
+         {
+            return Cp1252ToUtf8(varchar);
+         }
+         return DbValue {};
+      }
 
-  default:
-    dbData = WStringToUtf8(row.get<nanodbc::wide_string>(col));
-    return dbData;
-  }
+         // we have unicode !
+      case SQL_WCHAR:
+      case SQL_WVARCHAR:
+         return WStringToUtf8(row.get<nanodbc::wide_string>(col));
+
+      case SQL_WLONGVARCHAR:
+      {
+         auto longVarChar = row.get<nanodbc::wide_string>(col);
+         if (row.is_null(col) == false)
+            return WStringToUtf8(longVarChar);
+         return DbValue {};
+      }
+
+      case SQL_BIGINT:
+      case SQL_TINYINT:
+      case SQL_INTEGER:
+      case SQL_SMALLINT:
+         return row.get<std::int64_t>(col);
+
+      case SQL_FLOAT:
+      case SQL_REAL:
+      case SQL_DOUBLE:
+         return row.get<double>(col);
+
+      // for now, other types, ask odbc to get their string representation
+      // will need to be fixed as needs arise!
+      case SQL_NUMERIC:
+      case SQL_DECIMAL:
+      default:
+         return Cp1252ToUtf8(row.get<std::string>(col));
+   }
 }
 
-json::object RowToJsonObject(nanodbc::result &row) {
-  json::object rowData;
-  for (short colIdx = 0; colIdx < row.columns(); colIdx++) {
-    auto &jsonValue = rowData[row.column_name(colIdx)];
-    auto dbValue = GetColumnValue(row, colIdx);
-    std::visit(ToJsonValue{jsonValue}, dbValue);
-  }
+json::object RowToJsonObject(nanodbc::result& row)
+{
+   json::object                                   rowData;
+   std::vector<std::tuple<unsigned, std::string>> badCols;
+   for (short colIdx = 0; colIdx < row.columns(); colIdx++)
+   {
+      auto&   jsonValue = rowData[row.column_name(colIdx)];
+      DbValue dbValue;
+      try
+      {
+         dbValue = GetColumnValue(row, colIdx);
+      }
+      catch (std::exception& ex)
+      {
+         badCols.push_back(std::make_tuple(colIdx, ex.what()));
+      }
+      std::visit(ToJsonValue {jsonValue}, dbValue);
+   }
+   // if we got bad cols, report them
+   if (!badCols.empty())
+   {
+      std::string errorMsg = std::format("bad row: [{}]", json::serialize(rowData));
+      for (auto badCol: badCols)
+         errorMsg += std::format("\nError on column: {}, what: {}", row.column_name(std::get<0>(badCol)), std::get<1>(badCol));
+      throw std::runtime_error(errorMsg);
+   }
 
-  return rowData;
+   return rowData;
 }
 
 // could be used for a smaller output
 // at the expanse of readability & having to reconstruct objects
-json::value GetStructureOfArray(nanodbc::result rowIt) {
-  json::object data;
+json::value GetStructureOfArray(nanodbc::result rowIt)
+{
+   json::object data;
 
-  for (auto colIdx = 0; colIdx < rowIt.columns(); colIdx++) {
-    data[rowIt.column_name(colIdx)] = json::array{};
-  }
+   for (auto colIdx = 0; colIdx < rowIt.columns(); colIdx++)
+   {
+      data[rowIt.column_name(colIdx)] = json::array {};
+   }
 
-  int rowCount{0};
+   int rowCount {0};
 
-  while (rowIt.next()) {
-    rowCount++;
+   while (rowIt.next())
+   {
+      rowCount++;
 
-    for (auto colIdx = 0; colIdx < rowIt.columns(); colIdx++) {
-      // get Array to populate
-      auto &colArray = data[rowIt.column_name(colIdx)].as_array();
-      auto dbValue = GetColumnValue(rowIt, colIdx);
-      std::visit(ToJsonArray{colArray}, dbValue);
-    }
-  }
+      for (auto colIdx = 0; colIdx < rowIt.columns(); colIdx++)
+      {
+         // get Array to populate
+         auto& colArray = data[rowIt.column_name(colIdx)].as_array();
+         auto  dbValue  = GetColumnValue(rowIt, colIdx);
+         std::visit(ToJsonArray {colArray}, dbValue);
+      }
+   }
 
-  json::object object;
-  object["recordCount"] = rowCount;
-  object["data"] = data;
+   json::object object;
+   object["recordCount"] = rowCount;
+   object["data"]        = data;
 
-  return object;
+   return object;
 }
 
-json::array GetArrayOfStructure(nanodbc::result rowIt) {
-  json::array rows;
-  while (rowIt.next()) {
-    rows.push_back(RowToJsonObject(rowIt));
-  }
-  return rows;
+json::array GetArrayOfStructure(nanodbc::result rowIt)
+{
+   json::array rows;
+   while (rowIt.next())
+   {
+      rows.push_back(RowToJsonObject(rowIt));
+   }
+   return rows;
 }
 
-std::string readJsonFile(const std::string &filename) {
-  std::ifstream input(filename);
-  return std::string((std::istreambuf_iterator<char>(input)),
-                     std::istreambuf_iterator<char>());
+std::string readJsonFile(const std::string& filename)
+{
+   std::ifstream input(filename);
+   return std::string((std::istreambuf_iterator<char>(input)),
+                      std::istreambuf_iterator<char>());
 }
 
-int main(int argc, char **argv) {
-  std::string database{argv[1]};
-  auto connection_string =
+int main(int argc, char** argv)
+{
+   std::string database {argv[1]};
+   auto        connection_string =
       "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=" + database;
 
-  try {
-    nanodbc::connection conn(connection_string);
+   try
+   {
+      nanodbc::connection conn(connection_string);
 
-    json::object jsonDoc;
-    jsonDoc["version"] = "1.0.0";
-    json::object tables;
+      json::object jsonDoc;
+      jsonDoc["version"] = "1.0.0";
+      json::object tables;
 
-    for (auto tableInfo : g_tablesToExport) {
-      auto rowIt = nanodbc::execute(conn, tableInfo.extractQry);
-      // an array of object is more verbose, but easier to visualise and diff
-      tables[tableInfo.name] = GetArrayOfStructure(rowIt);
+      for (auto tableInfo: g_tablesToExport)
+      {
+         auto rowIt = nanodbc::execute(conn, tableInfo.extractQry);
+         // an array of object is more verbose, but easier to visualise and diff
+         tables[tableInfo.name] = GetArrayOfStructure(rowIt);
 
-      // structure of array would be more memory friendly, but less intuitive
-      // see https://en.wikipedia.org/wiki/AoS_and_SoA
-      // tables[tableInfo.name] = GetStructureOfArray(rowIt);
-    }
-
-    jsonDoc["TlgSchema"] = tables;
-
-    pretty_print(std::cout, jsonDoc);
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << '\n';
-  }
-  return 0;
+         // structure of array would be more memory friendly, but less intuitive
+         // see https://en.wikipedia.org/wiki/AoS_and_SoA
+         // tables[tableInfo.name] = GetStructureOfArray(rowIt);
+      }
+      jsonDoc["TlgSchema"] = tables;
+      if (argc > 2)
+      {
+         std::ofstream fileOut(argv[2]);
+         pretty_print(fileOut, jsonDoc);
+      }
+      else
+      {
+         pretty_print(std::cout, jsonDoc);
+      }
+   }
+   catch (const std::exception& e)
+   {
+      std::cerr << e.what() << '\n';
+   }
+   return 0;
 }
--- JSon2Access.cpp
+++ JSon2Access.cpp
@@ -1,47 +1,126 @@
-#include <nanodbc/nanodbc.h>
 #include <boost/json.hpp>
+#include <nanodbc/nanodbc.h>
 
 #include <fstream>
 #include <iostream>
+#include <set>
 
 namespace json = boost::json;
 
 std::string readJsonFile(const std::string& filename)
 {
-    std::ifstream input(filename);
-    return std::string ((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
+   std::ifstream input(filename);
+   return std::string((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
 }
 
-int main(int argc, char **argv)
+struct ColumnDef
 {
-    try
-    {
-        std::string jsonData = readJsonFile(argv[1]);
-        auto jsonDoc = json::parse(jsonData);
-        auto version = jsonDoc.at("version");
-        auto schema = jsonDoc.at("TlgSchema");
+   std::string tag;
+   std::string column;
+};
 
-        auto const& obj = schema.get_object();
-        if(! obj.empty())
-        {
-            auto it = obj.begin();
-            for(;;)
+class JsonImport
+{
+public:
+   virtual std::string         GetEraseCmd()  = 0;
+   virtual std::string         GetTableName() = 0;
+   virtual std::string         GetJsonName()  = 0;
+   virtual std::set<ColumnDef> GetColumnDef() = 0;
+};
+#if 0
+struct JSonImport
+{
+   std::string              tag;
+   std::string              table;
+   std::string              removeTableDataCmd;
+   std::vector<TagToColumn> tagMapping;
+};
+
+std::vector<JSonImport> g_importInfo = {{"Tags",
+                                         "Tags",
+                                         "DELETE FROM Tags",
+                                         {
+                                            {"Tag_Code", nullptr},
+                                            {"Tag_Code", nullptr},
+                                            {"Tag_Code", nullptr},
+                                         }},
+                                        {"HeaderMessages", "LoggerMessages", ""}};
+#endif
+
+
+std::vector<std::string> g_tablesToErase = {
+   "DELETE FROM LoggerMessages",
+   "DELETE FROM Messages",
+   "DELETE FROM LogFiles",
+   "DELETE FROM Fields",
+   "DELETE FROM Tags",
+};
+
+int main(int argc, char** argv)
+{
+   try
+   {
+      std::string database {argv[1]};
+      auto        connection_string = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=" + database;
+      std::string jsonData          = readJsonFile(argv[2]);
+
+      auto jsonDoc = json::parse(jsonData);
+
+      nanodbc::connection conn(connection_string);
+
+      for (auto sqlCmd: g_tablesToErase)
+      {
+         auto rowIt = nanodbc::execute(conn, sqlCmd);
+         if (rowIt.has_affected_rows())
+         {
+            std::cout << "deleted row(s): " << rowIt.affected_rows() << std::endl;
+         }
+      }
+
+      auto        version = jsonDoc.at("version");
+      auto        schema  = jsonDoc.at("TlgSchema");
+      auto const& tables  = schema.get_object();
+
+      if (!tables.empty())
+      {
+         for (auto table = tables.begin(); table != tables.end(); ++table)
+         {
+            auto const& rows = table->value().get_array();
+            if (!rows.empty())
             {
-                std::cout <<  json::serialize(it->key()) << " : " << std::endl;
-                if(++it == obj.end())
-                    break;
-                std::cout << ",\n";
+               std::ostringstream cmdHeader;
+               cmdHeader << "insert into " << table->key_c_str() << "(";
+
+               for (size_t idx = 0; idx < rows.size(); idx++)
+               {
+                  auto row = rows[idx].get_object();
+                  if (row.empty() == false)
+                  {
+                     std::ostringstream sqlColumns;
+                     std::ostringstream sqlValues;
+                     for (auto it = row.begin(); it != row.end(); ++it)
+                     {
+                        if (it != row.begin())
+                        {
+                           sqlColumns << ", ";
+                           sqlValues << ", ";
+                        }
+                        sqlColumns << it->key_c_str();
+                        sqlValues << it->value();
+                     }
+                     std::ostringstream sqlCmd;
+                     sqlCmd << cmdHeader.str() << sqlColumns.str() << ") VALUES(" << sqlValues.str() << ")";
+                     std::cout << sqlCmd.str() << std::endl;
+                  }
+               }
             }
-        }
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+         }
+      }
+   }
+   catch (const std::exception& e)
+   {
+      std::cerr << e.what() << '\n';
+   }
 
-    return 0;
-    std::string database {NANODBC_TEXT(argv[2])};
-    auto connection_string {NANODBC_TEXT("Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=") + database};
-
-    return 0;
+   return 0;
 }
